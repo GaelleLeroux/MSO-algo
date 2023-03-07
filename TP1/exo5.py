8b9bda466db7efadab04ab2253ee7515a404c0c8
Gaelle Leroux <gaelleleroux974@gmail.com>
2023-03-07 15:09:39 +0100

    deff

--- TP1/exo5.py
+++ TP1/exo5.py
@@ -4,11 +4,6 @@ class Noeuds :
     def __init__(self,x) :
         self.valeur = x
         self.suivant = None
-    
-
-    
-
-
 
 class ListeChainee :
     def __init__(self) :
@@ -54,8 +49,9 @@ class ListeChainee :
             self.fin.suivant = tempo.suivant
             self.taille-=1
         elif k>=self.taille :
+            self.taille-=1
             tempo = self.fin.suivant
-            for i in range(self.taille-1):
+            for i in range(self.taille):
                 self.fin = self.fin.suivant
             self.fin.suivant = tempo
         else :
@@ -63,7 +59,7 @@ class ListeChainee :
             tempo = self.fin
             tempo2 = tempo.suivant
             trouver = True
-            for i in range(0,k-1) :
+            for i in range(0,k) :
                 if tempo2.suivant == None :
                     tempo.suivant = None
                     trouver = False
@@ -79,6 +75,8 @@ class ListeChainee :
         if self.fin == None :
             print("Liste vide")
         else :
+            print("La premier nombre de la liste est considéré comme étant à la ligne 1 : ")
+            tempo = self.fin.suivant
             i = 1
             while (i<self.taille) :
                 if (tempo.valeur == valeur):
@@ -112,22 +110,57 @@ class ListeChainee :
         else :
             return False
 
+    def creaction_cercle(self,nombre) :
+        while (nombre!=0):
+            self.inserer(nombre,0)
+            nombre-=1
+            
+    def survivant(self,numero):
+        if self.fin == None :
+            return "Liste vide, pas de cercle existant"
+        else :
+            tempo = self.fin.suivant
+            for i in range(0,numero-1) :
+                tempo = tempo.suivant
+            replique = self.taille
+            while(replique != 1):
+                for i in range(3):
+                    tempo = tempo.suivant
+                    while(tempo.valeur==None):
+                        tempo = tempo.suivant
+                tempo.valeur = None
+                replique-=1
+            tempo = tempo.suivant
+            while(tempo.valeur==None):
+                tempo = tempo.suivant
+            print("Le dernier survivant est : " + str(tempo.valeur))
 
+    def survivant2(self,numero):
+        if self.fin == None :
+            return "Liste vide, pas de cercle existant"
+        else :
+            while(self.taille != 1):
+                for i in range(2):
+                    numero+=1
+                self.supprimer((numero)%50)
+            print("Le dernier survivant est : " + str(self.fin.valeur))
 
 
 
 a = ListeChainee()
-print("Test liste vide :")
+print("Test creation cercle")
+a.creaction_cercle(50)
+a.supprimer(5)
 a.afficher()
-a.inserer(0,0)
-a.inserer(1,1)
-a.inserer(2,2)
-a.inserer(3,3)
-print("Test insertion")
-a.afficher()
-print("Test suppresion")
-a.supprimer(4)
-a.afficher()
+print("Test survivant")
+a.survivant(46)
+
+b = ListeChainee()
+b.creaction_cercle(50)
+print("Test survivant2222222222222222")
+b.survivant2(46)
+b.afficher()
+
 
         
 
