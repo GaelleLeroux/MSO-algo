8b6ad11be5f026c42e0dd9a644a750c561e28d91
Gaelle Leroux <gaelleleroux974@gmail.com>
2023-03-19 11:48:36 +0100

    fin dicho

--- TP3_recursif/dicho.py
+++ TP3_recursif/dicho.py
@@ -1,23 +1,23 @@
 
 import numpy as np
 
-def dicho(liste,k):
+def dicho(liste,listeV,k):
     liste = np.sort(liste)
     if (len(liste)==1):
         if (liste[0]==k):
-            return True
+            return listeV.index(k)
         else :
-            return False
+            return -1
     else :
         if (liste[len(liste)//2]<k):
-            return (dicho(liste[len(liste)//2+1:],k))
+            return (dicho(liste[len(liste)//2+1:],listeV,k))
         elif (liste[len(liste)//2]>k):
-            return (dicho(liste[:len(liste)//2],k))
+            return (dicho(liste[:len(liste)//2],listeV,k))
         else :
-            return True
+            return listeV.index(k)
         
 
 test = [17,11,33,25,48,18,6]
-t = dicho(test,60)
+t = dicho(test,test,18)
 print(t)
         
