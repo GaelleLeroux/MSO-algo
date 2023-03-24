8a486c0e79efdce9cf1e29ac1f73e111a68235eb
Gaelle Leroux <gaelleleroux974@gmail.com>
2023-03-24 13:39:42 +0100

    gg

--- TP3_recursif/Hanoi.py
+++ TP3_recursif/Hanoi.py
@@ -4,5 +4,5 @@ def Hanoi(n,D,A,I):
         print("Déplacer le disque de "+str(D)+ " vers " +str(A))
         Hanoi(n-1, I, A, D)
 
-Hanoi(3,1,2,3)
+Hanoi(3,1,3,2) #nombre de disques utilisés, emplacement de départ,emplacement d'arrivée,emplacement intermédiaire.
         
