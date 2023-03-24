8855587655951587a20bb16163ee5f3f33028b9e
Gaelle Leroux <gaelleleroux974@gmail.com>
2023-03-24 15:17:05 +0100

    fin TP3

--- TP3_recursif/Hanoi.py
+++ TP3_recursif/Hanoi.py
@@ -5,4 +5,15 @@ def Hanoi(n,D,A,I):
         Hanoi(n-1, I, A, D)
 
 Hanoi(3,1,3,2) #nombre de disques utilisés, emplacement de départ,emplacement d'arrivée,emplacement intermédiaire.
+  
+print("Mon hanoi : ////////////////////////////////:")      
         
+def Ha(n,D,A,I):
+    if n==1 :
+        print("Déplacer le disque de "+str(D)+ " vers " +str(A))
+    else :
+        Ha(n-1,D,I,A)
+        Ha(1,D,A,I)
+        Ha(n-1,I,A,D)
+        
+Ha(3,1,3,2) #nombre de disques utilisés, emplacement de départ,emplacement d'arrivée,emplacement intermédiaire.
--- TP3_recursif/dicho.py
+++ TP3_recursif/dicho.py
@@ -2,22 +2,23 @@
 import numpy as np
 
 def dicho(liste,listeV,k):
-    liste = np.sort(liste)
-    if (len(liste)==1):
+    n = len(liste);
+    if (n==1):
         if (liste[0]==k):
             return listeV.index(k)
         else :
             return -1
     else :
-        if (liste[len(liste)//2]<k):
-            return (dicho(liste[len(liste)//2+1:],listeV,k))
-        elif (liste[len(liste)//2]>k):
-            return (dicho(liste[:len(liste)//2],listeV,k))
+        if (liste[n//2]<k):
+            return (dicho(liste[n//2+1:],listeV,k))
+        elif (liste[n//2]>k):
+            return (dicho(liste[:n//2],listeV,k))
         else :
             return listeV.index(k)
         
 
 test = [17,11,33,25,48,18,6]
-t = dicho(test,test,18)
+liste = np.sort(test)
+t = dicho(liste,test,17)
 print(t)
         
