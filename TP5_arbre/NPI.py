9c24aed8eeba5430d725e66b8d3a9ea519d98785
Gaelle Leroux <gaelleleroux974@gmail.com>
2023-03-31 18:08:50 +0200

    de

--- TP5_arbre/NPI.py
+++ TP5_arbre/NPI.py
@@ -8,22 +8,27 @@ def cal(u,nmb1,nmb2)  :
         nmb1 = nmb1 * nmb2
     if (u=="**"):
         nmb1 = nmb1 ** nmb2
+    if (u=="-"):
+        nmb1 = nmb1 - nmb2
     return nmb1
       
 def NPI(s):
     l = s.split()
-    operateur = ["+","/","*","**"]
+    operateur = ["+","/","*","**","-","#"]
     nmb = []
     for i in range(0,len(l)):
         u = l[i]
         if u in operateur:
-            nmb1 = nmb.pop()
-            nmb2 = nmb.pop()
-            nmb.append(cal(u,nmb1,nmb2))
-                
+            if u=="#":
+                nmb1 = nmb.pop()
+                nmb.append(-nmb1)
+            else:
+                nmb2 = nmb.pop()
+                nmb1 = nmb.pop()
+                nmb.append(cal(u,nmb1,nmb2))    
         else :
             nmb.append(int(u))
     print(nmb[0])
     
         
-NPI("2 3 * 4 5 * +")        
+NPI("5 # 3 * 3 4 * +")        
